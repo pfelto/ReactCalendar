527b6b5d258da7648afd13a3126dd17f8a824ced
Paul Felton <paul.r.felton@gmail.com>
2020-06-08 09:49:33 -0500

    Deleted Default Code and added top level component and index.js to call

---
+++ src/CalendarApp.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class CalendarApp extends React.Component {
+    render() {
+      return (
+        <h1>Hello, world!</h1>
+      );
+    }
+  }
+  
+export default CalendarApp;
